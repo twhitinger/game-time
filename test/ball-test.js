3cadb363a21d09772c6f15efa50ae0347f0dc147
Chris Concannon <concach00@gmail.com>
2016-10-11 15:11:01 -0600

    figuring out ball movement animation

--- test/ball-test.js
+++ test/ball-test.js
@@ -11,4 +11,16 @@ describe('Ball', function() {
       assert.isObject(ball);
     });
   });
+
+  context('movement', function() {
+    let ball = new Ball();
+    it('should have the ability to move', function() {
+      assert.isFunction(ball.move, "move is NOT a function");
+    });
+    it('should move to a new location', function() {
+      ball.move({v: [2, 2], r: 1});
+      assert.notEqual(ball.x, 50);
+      assert.notEqual(ball.y, 50);
+    });
+  });
 });
